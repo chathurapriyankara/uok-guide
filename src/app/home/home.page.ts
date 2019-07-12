a1c2a4aeea51fa3147981fe962d44ef658774b42
chathura <chathura@localhost.localdomain>
2019-07-12 14:32:50 -0400

    First

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  map: Map;
 
-  constructor() {}
+  ionViewDidEnter() { this.leafletMap(); }
 
+  leafletMap() {
+    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+
+    marker([28.6, 77]).addTo(this.map)
+        .bindPopup('Ionic 4 <br> Leaflet.')
+        .openPopup();
+  }
 }
